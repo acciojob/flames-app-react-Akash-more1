b4ff9b78682f272707982d0a52081d32331bb81d
Akash-more1 <akashmore9725@gmail.com>
2024-07-29 20:16:15 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,117 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App =()=> {
+    const [input1, setInput1] = useState(""); 
+    const[input2, setInput2] = useState("");
+    const[result, setResult] = useState("");
+
+    const handleInput1= (e)=>{
+        setInput1(e.target.value);
+    }
+
+    const handleInput2= (e)=>{
+        setInput2(e.target.value);
+    }
+
+    const relCalulate= ()=>{
+
+        if(input1=="" || input2==""){
+            setResult("Please Enter valid input");
+            return;
+        }
+       const map1={};
+
+       const map2={};
+
+       for(const char of input1){
+        map1[char] = (map1[char] || 0) + 1;
+       }
+
+       for(const char of input2){
+        map2[char] = (map2[char] || 0) + 1;
+       }
+    
+       let remaining1= [];
+       let remaining2 = [];
+
+       for(const char of input1){
+        if(map2[char]){
+            map2[char]--;
+            if(map2[char]== 0){
+                delete map2[char];
+            }
+        }
+        else{
+            remaining1.push(char);
+        }
+       }
+
+       for(const char of input2){
+        if(map1[char]){
+            map1[char]--;
+            if(map1[char]== 0){
+                delete map1[char];
+            }
+        }
+        else{
+            remaining2.push(char);
+        }
+       }
+        
+       console.log(remaining1.length);
+       console.log(remaining1.join(''));
+
+       console.log(remaining2.join('middle'));
+
+       console.log(remaining2.join(''));
+       console.log((remaining2.length+remaining2.length));
+       console.log((remaining2.length+remaining2.length)%6);
+       setResult((remaining1.length+remaining2.length)%6);
+        let leth =(remaining1.length+remaining2.length)%6;
+       if(leth==0){
+        setResult("Siblings");
+       }
+       else if(leth==1){
+        setResult("Friends");
+       }
+       else if(leth==2){
+        setResult("Love");
+       }
+       else if(leth==3){
+        setResult("Affection");
+       }
+       else if(leth==4){
+        setResult("Marriage");
+       }
+       else if(leth==5){
+        setResult("Enemy");
+       }
+
+    }
+
+    const clear = ()=>{
+
+        let in1=document.getElementById("ik");
+        let in2=document.getElementById("il");
+        in1.value="";
+        in2.value="";
+         setInput1("");
+         setInput2("");
+        setResult("");
+    }
+
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <input id="ik" onChange={handleInput1}/>
+               <input id="il" onChange={handleInput2}/>
+               <button id="in" onClick={relCalulate}>Calculate Relationship Future</button>
+               <button id="io" onClick={clear}>Clear</button>
+               <h3>{result}</h3>
             </div>
         )
-    }
+    
 }
 
 
